1f13d717f0a74f2d04af374575bc5617bb9f59be
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-02 15:21:36 +0700

    Add project files.

---
+++ Dimas Damar Buana_20210140135_Exe2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimas_Damar_Buana_20210140135_Exe2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
