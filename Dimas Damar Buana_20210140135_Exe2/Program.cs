1c9a2a000037c9cf86c71cb3b9d65b6593ef01d9
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-02 15:37:03 +0700

    add variables and array

--- Dimas Damar Buana_20210140135_Exe2/Program.cs
+++ Dimas Damar Buana_20210140135_Exe2/Program.cs
@@ -8,6 +8,10 @@ namespace Dimas_Damar_Buana_20210140135_Exe2
 {
     class Program
     {
+        int[] dimas = new int[28];
+
+        int n;
+
         static void Main(string[] args)
         {
         }
