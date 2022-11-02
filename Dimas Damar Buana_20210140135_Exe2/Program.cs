9ea8e045ed75f15f2dee382f76d246bab7e0a562
Dimas Damar Buana <dimas.damar.ft21@mail.umy.ac.id>
2022-11-02 16:38:07 +0700

    add method for Merge sort

--- Dimas Damar Buana_20210140135_Exe2/Program.cs
+++ Dimas Damar Buana_20210140135_Exe2/Program.cs
@@ -55,8 +55,50 @@ namespace Dimas_Damar_Buana_20210140135_Exe2
                 }
             }
         }
+
         
-        
+
+        public void MainMerge(int low, int mid, int high, int[] dimas)
+        {
+            int i = low;
+            int DB = (mid - 1);
+            int k = (high - low + 1);
+            int pos;
+            int[] temp = new int[28];
+            
+            
+
+            while ((low <= DB) && (mid <= high))
+            {
+                if (dimas[low] <= dimas[mid])
+                    temp[pos++] = dimas[low++];
+                else
+                    temp[pos++] = dimas[mid++];
+            }
+            while (low <= DB)
+                temp[pos++] = dimas[low++];
+            while (mid <= high)
+                temp[pos++] = dimas[mid++];
+            for (i = 0; i < k; i++)
+            {
+                dimas[high] = temp[high];
+                high--;
+            }
+        }
+        public void SortMerge(int[] dimas,int low, int high)
+        {
+            int mid;
+            if (low > high)
+            {
+                mid = (low + high) / 2;
+                SortMerge(dimas, low, mid);
+                SortMerge(dimas, (mid + 1), high);
+                MainMerge(dimas, low, (mid + 1), high)
+            }
+        }
+
+
+
 
         static void Main(string[] args)
         {
